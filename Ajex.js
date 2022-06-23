d8e8eabdecbac9a543b9b8b5430bcdc3c1e9da0f
praju-nikam <sawanprajakta84@gmail.com>
2022-06-23 10:40:07 +0530

    Ajax Demo and Promise

--- Ajex.js
+++ Ajex.js
@@ -1,3 +1,5 @@
+// UC-2-Ajax Demo and Method Callback
+
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function makeAJAXCall(methodType, url, callback, async = true, data = null) {
@@ -44,3 +46,48 @@ function userAdded(data) {
     console.log("User Added: " + data);
 }
 makeAJAXCall("POST", postURL, userAdded, true, emplData);
+
+// UC-3-Ajax Demo and Promise
+
+function makeAJAXPromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = function() {
+            console.log("State Changed called. Ready State : " + xhr.readyState + " status:" + xhr.status);
+            if (xhr.status.toString().match('^[2][0-9]{2}$')) {
+                resolve(xhr.responseText);
+            } else if (xhr.status.toString().match('^[4,5][0-9]{2}$')) {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("XHR Failed");
+            }
+        }
+        xhr.open(methodType, url, async);
+        if (data) {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        } else {
+            xhr.send();
+            console.log(methodType + " request to sent the server");
+        }
+    });
+}
+//const getURL = "http://127.0.0.1:5500/employees/1"
+makeAJAXPromiseCall("GET", getURL, true).then(responseText => {
+    console.log("Get user Data : " + responseText)
+}).catch(error => console.log("Get Error Status : " + JSON.stringify(error)));
+
+//const deleteURL = "http://localhost:5500/employees/4";
+
+makeAJAXPromiseCall("DELETE", deleteURL, false).then(responseText => {
+    console.log("User Deleted : " + responseText)
+}).catch(error => console.log("Delete Error Status : " + JSON.stringify(error)));
+
+//const postURL = "http://localhost:5500/employees";
+//const emplData = { "name": "Swara", "salary": "40000" };
+makeAJAXPromiseCall("POST", postURL, false).then(responseText => {
+    console.log("User Added : " + responseText)
+}).catch(error => console.log("Post Error Status : " + JSON.stringify(error)));
